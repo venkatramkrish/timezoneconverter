e32deb75f58c5438dc3e9b07e67e09a054738e6e
venkatr86-coder <venkatr86@gmail.com>
2026-08-13 09:37:21 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Timeband service worker
    Bump CACHE when you change index.html so phones pick up the new version. */
-var CACHE = "timeband-v1";
+var CACHE = "timeband-v2";
 
 var SHELL = [
   "./",
