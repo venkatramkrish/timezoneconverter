145ebce5a0bcc401b6215cb9970bb46111e1e1d8
venkatramkrish <316117916+venkatramkrish@users.noreply.github.com>
2026-08-13 11:51:13 +0530

    Bugfixes and list view added

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Timeband service worker
    Bump CACHE when you change index.html so phones pick up the new version. */
-var CACHE = "timeband-v4";
+var CACHE = "timeband-v21";
 
 var SHELL = [
   "./",
